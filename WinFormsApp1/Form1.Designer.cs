1cc3e097c4f7770a3dd21237b6289ac132b2b905
Orxan477 <tu201906147@code.edu.az>
2023-08-15 01:29:30 +0400

    V5

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -34,16 +34,22 @@
             txtQRCode = new TextBox();
             reng = new TextBox();
             pic = new PictureBox();
+            label2 = new Label();
+            Gradient = new CheckBox();
+            openFileDialog1 = new OpenFileDialog();
+            button1 = new Button();
+            image_check = new Label();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)pic).BeginInit();
             SuspendLayout();
             // 
             // btnGenerate
             // 
-            btnGenerate.Location = new Point(769, 588);
+            btnGenerate.Location = new Point(816, 570);
             btnGenerate.Name = "btnGenerate";
-            btnGenerate.Size = new Size(75, 23);
+            btnGenerate.Size = new Size(75, 38);
             btnGenerate.TabIndex = 0;
-            btnGenerate.Text = "Generate";
+            btnGenerate.Text = "Düzəltmək";
             btnGenerate.UseVisualStyleBackColor = true;
             btnGenerate.Click += btnGenerate_Click;
             // 
@@ -60,15 +66,15 @@
             // txtQRCode
             // 
             txtQRCode.BackColor = Color.White;
-            txtQRCode.Location = new Point(12, 503);
+            txtQRCode.Location = new Point(12, 529);
             txtQRCode.Multiline = true;
             txtQRCode.Name = "txtQRCode";
-            txtQRCode.Size = new Size(832, 79);
+            txtQRCode.Size = new Size(503, 79);
             txtQRCode.TabIndex = 3;
             // 
             // reng
             // 
-            reng.Location = new Point(12, 588);
+            reng.Location = new Point(521, 553);
             reng.Name = "reng";
             reng.Size = new Size(100, 23);
             reng.TabIndex = 4;
@@ -78,15 +84,73 @@
             pic.BackColor = Color.White;
             pic.Location = new Point(12, 27);
             pic.Name = "pic";
-            pic.Size = new Size(832, 470);
+            pic.Size = new Size(879, 496);
             pic.SizeMode = PictureBoxSizeMode.CenterImage;
             pic.TabIndex = 2;
             pic.TabStop = false;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(521, 532);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Rəng kodu";
+            // 
+            // Gradient
+            // 
+            Gradient.AutoSize = true;
+            Gradient.Location = new Point(632, 532);
+            Gradient.Name = "Gradient";
+            Gradient.Size = new Size(71, 19);
+            Gradient.TabIndex = 6;
+            Gradient.Text = "Gradient";
+            Gradient.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(521, 582);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 26);
+            button1.TabIndex = 8;
+            button1.Text = "Logo yüklə";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // image_check
+            // 
+            image_check.AutoSize = true;
+            image_check.ForeColor = Color.FromArgb(0, 192, 0);
+            image_check.Location = new Point(602, 588);
+            image_check.Name = "image_check";
+            image_check.Size = new Size(0, 15);
+            image_check.TabIndex = 9;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Enabled = false;
+            label3.Location = new Point(764, 508);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 15);
+            label3.TabIndex = 10;
+            label3.Visible = false;
+            // 
             // Form1
             // 
             AutoScaleMode = AutoScaleMode.Inherit;
-            ClientSize = new Size(856, 623);
+            ClientSize = new Size(903, 620);
+            Controls.Add(label3);
+            Controls.Add(image_check);
+            Controls.Add(button1);
+            Controls.Add(Gradient);
+            Controls.Add(label2);
             Controls.Add(reng);
             Controls.Add(txtQRCode);
             Controls.Add(pic);
@@ -94,6 +158,7 @@
             Controls.Add(btnGenerate);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "QRCode Generate";
             ((System.ComponentModel.ISupportInitialize)pic).EndInit();
             ResumeLayout(false);
@@ -107,5 +172,11 @@
         private TextBox txtQRCode;
         private TextBox reng;
         private PictureBox pic;
+        private Label label2;
+        private CheckBox Gradient;
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private Label image_check;
+        private Label label3;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.VisualBasic.ApplicationServices;
 using QRCoder;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private Bitmap logo;
         public Form1()
         {
             InitializeComponent();
@@ -19,41 +22,17 @@ namespace WinFormsApp1
         {
             //globalda olan
 
-            QRCodeGenerator qr = new QRCodeGenerator();
-            QRCodeData data = qr.CreateQrCode(txtQRCode.Text, QRCodeGenerator.ECCLevel.Q);
-            QRCode code = new QRCode(data);
-            System.Drawing.Bitmap qrImage = code.GetGraphic(5);
-
-            if (reng.Text == "")
-            {
-                reng.Text = "#000";
-            }
-
-            System.Drawing.Color customColor = System.Drawing.ColorTranslator.FromHtml(reng.Text);
-
-            for (int y = 0; y < qrImage.Height; y++)
-            {
-                for (int x = 0; x < qrImage.Width; x++)
-                {
-                    System.Drawing.Color pixelColor = qrImage.GetPixel(x, y);
-                    if (pixelColor.ToArgb() == System.Drawing.Color.Black.ToArgb())
-                    {
-                        qrImage.SetPixel(x, y, customColor); // Siyah pikselleri özel renkle değiştir
-                    }
-                }
-            }
-
-            pic.Image = qrImage;
-
-            //soldan saga
-
             //QRCodeGenerator qr = new QRCodeGenerator();
             //QRCodeData data = qr.CreateQrCode(txtQRCode.Text, QRCodeGenerator.ECCLevel.Q);
             //QRCode code = new QRCode(data);
             //System.Drawing.Bitmap qrImage = code.GetGraphic(5);
 
-            //System.Drawing.Color startColor = System.Drawing.ColorTranslator.FromHtml("#3f4293"); // Başlangıç rengi
-            //System.Drawing.Color endColor = System.Drawing.ColorTranslator.FromHtml("#3d3d3b"); // Bitiş rengi
+            //if (reng.Text == "")
+            //{
+            //    reng.Text = "#000";
+            //}
+
+            //System.Drawing.Color customColor = System.Drawing.ColorTranslator.FromHtml(reng.Text);
 
             //for (int y = 0; y < qrImage.Height; y++)
             //{
@@ -62,15 +41,14 @@ namespace WinFormsApp1
             //        System.Drawing.Color pixelColor = qrImage.GetPixel(x, y);
             //        if (pixelColor.ToArgb() == System.Drawing.Color.Black.ToArgb())
             //        {
-            //            int gradientValue = (int)((double)x / qrImage.Width * 255); // Gradient değerini hesapla
-            //            System.Drawing.Color gradientColor = System.Drawing.Color.FromArgb(gradientValue, endColor); // Gradient rengi oluştur
-
-            //            qrImage.SetPixel(x, y, gradientColor); // Siyah pikselleri gradient rengi ile değiştir
+            //            qrImage.SetPixel(x, y, customColor); // Siyah pikselleri özel renkle değiştir
             //        }
             //    }
             //}
 
             //pic.Image = qrImage;
+
+
 
             // yuxardan asagi
 
@@ -101,48 +79,80 @@ namespace WinFormsApp1
 
             // logo ile
 
-            //QRCodeGenerator qr = new QRCodeGenerator();
-            //QRCodeData data = qr.CreateQrCode(txtQRCode.Text, QRCodeGenerator.ECCLevel.Q);
-            //QRCode code = new QRCode(data);
-            //Bitmap qrImage = code.GetGraphic(5);
+            QRCodeGenerator qr = new QRCodeGenerator();
+            QRCodeData data = qr.CreateQrCode(txtQRCode.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode code = new QRCode(data);
+            Bitmap qrImage = code.GetGraphic(5);
+            if (reng.Text == "")
+            {
+                reng.Text = "#000";
+            }
+            Color startColor = ColorTranslator.FromHtml(reng.Text); // Başlangıç rengi
+                                                                    //Color startColor = ColorTranslator.FromHtml("#3f4293"); // Başlangıç rengi
+            if (label3.Text != "")
+            {
+                logo = new Bitmap(@"C:\Users\Orxan477\Desktop\QRCode_Generate\" + label3.Text); // Logo dosyasının yolunu belirtin
+                int logoSize = qrImage.Width / 2; // Logo boyutunu ayarlayabilirsiniz
+                logo = new Bitmap(logo, new Size(logoSize, logoSize));
 
-            //Color startColor = ColorTranslator.FromHtml("#3f4293"); // Başlangıç rengi
+            }
 
-            //Bitmap logo = new Bitmap(@"C:\Users\Orxan477\Desktop\QRCode_Generate\şenbay-beyaz.png"); // Logo dosyasının yolunu belirtin
-            //int logoSize = qrImage.Width / 2; // Logo boyutunu ayarlayabilirsiniz
-            //logo = new Bitmap(logo, new Size(logoSize, logoSize));
-
-            //for (int y = 0; y < qrImage.Height; y++)
-            //{
-            //    for (int x = 0; x < qrImage.Width; x++)
-            //    {
-            //        Color pixelColor = qrImage.GetPixel(x, y);
-            //        if (pixelColor.ToArgb() == Color.Black.ToArgb())
-            //        {
-            //            int gradientValue = (int)((double)y / qrImage.Height * 255 * 3); // Alpha değeri 255'ten fazla olmamalı
-            //            gradientValue = Math.Min(255, gradientValue); // Alpha değerini 255'e sınırla
-            //            Color gradientColor = Color.FromArgb(gradientValue, startColor);
+            for (int y = 0; y < qrImage.Height; y++)
+            {
+                for (int x = 0; x < qrImage.Width; x++)
+                {
+                    Color pixelColor = qrImage.GetPixel(x, y);
+                    if (Gradient.Checked == true)
+                    {
+                        if (pixelColor.ToArgb() == Color.Black.ToArgb())
+                        {
+                            int gradientValue = (int)((double)y / qrImage.Height * 255 * 3); // Alpha değeri 255'ten fazla olmamalı
+                            gradientValue = Math.Min(255, gradientValue); // Alpha değerini 255'e sınırla
+                            Color gradientColor = Color.FromArgb(gradientValue, startColor);
 
 
-            //            qrImage.SetPixel(x, y, gradientColor);
-            //        }
-            //    }
-            //}
+                            qrImage.SetPixel(x, y, gradientColor);
+                        }
+                    }
+                    else
+                    {
+                        if (pixelColor.ToArgb() == System.Drawing.Color.Black.ToArgb())
+                        {
+                            qrImage.SetPixel(x, y, startColor); // Siyah pikselleri özel renkle değiştir
+                        }
+                    }
+                }
+            }
+            if (label3.Text != "")
+            {
+                int xPos = (qrImage.Width - logo.Width) / 2;
+                int yPos = (qrImage.Height - logo.Height) / 2;
+                using (Graphics g = Graphics.FromImage(qrImage))
+                {
+                    g.DrawImage(logo, new Point(xPos, yPos));
+                }
+            }
 
-            //int xPos = (qrImage.Width - logo.Width) / 2;
-            //int yPos = (qrImage.Height - logo.Height) / 2;
-            //using (Graphics g = Graphics.FromImage(qrImage))
-            //{
-            //    g.DrawImage(logo, new Point(xPos, yPos));
-            //}
+            pic.Image = qrImage;
+        }
 
-            //pic.Image = qrImage;
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            OpenFileDialog opnfd = new OpenFileDialog();
+            opnfd.Filter = "Image Files|*.jpg;*.png;*.gif;*.bmp;*.jpeg|All Files|*.*";
 
+            if (opnfd.ShowDialog() == DialogResult.OK)
+            {
+                string imagePath = opnfd.FileName;
+                Bitmap image = new Bitmap(imagePath);
+                image_check.Text = "Yükləndi";
+                label3.Text = opnfd.SafeFileName;
+            }
         }
     }
 }
